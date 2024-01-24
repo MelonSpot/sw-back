602c0c7915c002307560159152c4e585b23b4965
peterlah <peterlah@naver.com>
2024-01-24 23:19:17 +0900

    FEAT: 태그별 뮤직조회 기능 추가

--- repositories/music.repository.js
+++ repositories/music.repository.js
@@ -2,11 +2,14 @@ const Music = require("../schemas/music");
 
 class MusicRepository {
     getMusic = async (condition) => {
-        if (condition === "all") {
+        if (condition.type === "all") {
             const music = await Music.find();
             return music;
-        } else if (condition !== "all") {
-            const music = await Music.findOne({ _id: condition });
+        } else if (condition.type === "_id") {
+            const music = await Music.findOne({ _id: condition.musicId });
+            return music;
+        } else if (condition.type === "tag") {
+            const music = await Music.find({ tags: condition.tagName });
             return music;
         }
     };
